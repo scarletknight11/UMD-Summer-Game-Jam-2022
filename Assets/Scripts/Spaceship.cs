f5e7c2943eea84e6814f80067cb2a73f700cc63e
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-08-13 15:11:10 -0400

    got spaceship mechanics implemented. ship can rotate through yaw, pitch & roll

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Spaceship : MonoBehaviour {
+
+    [SerializeField]
+    private float yawTorque = 500f;
+    [SerializeField]
+    private float pitchTorque = 1000f;
+    [SerializeField]
+    private float rollTorque = 1000f;
+    [SerializeField]
+    private float thrust = 100f;
+    [SerializeField]
+    private float upThrust = 50f;
+    [SerializeField]
+    private float strafeThrust = 50f;
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float thrustGlideReduction = 0.999f;
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float upDownGlideReduction = 0.111f;
+    [SerializeField, Range(0.001f, 0.999f)]
+    private float leftRightGlideReduction = 0.111f;
+
+    Rigidbody rb;
+
+    // Input Values
+    private float thrust1D;
+    private float upDown1D;
+    private float strafe1D;
+    private float roll1D;
+    private Vector2 pitchYaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        // Rolll computation
+        rb.AddRelativeTorque(Vector3.back * roll1D * rollTorque * Time.deltaTime);
+        // pitch computation
+        rb.AddRelativeTorque(Vector3.right * Mathf.Clamp(-pitchYaw.y, -1f, 1f) * pitchTorque * Time.deltaTime);
+        //Yaw computation
+        rb.AddRelativeTorque(Vector3.up * Mathf.Clamp(pitchYaw.x, -1f, 1f) * yawTorque * Time.deltaTime);
+    }
+
+    #region Input Methods
+
+    public void OnThrust(InputAction.CallbackContext context)
+    {
+        thrust1D = context.ReadValue<float>();
+    }
+
+    public void OnStrafe(InputAction.CallbackContext context)
+    {
+        strafe1D = context.ReadValue<float>();
+    }
+
+    public void OnUpDown(InputAction.CallbackContext context)
+    {
+        upDown1D = context.ReadValue<float>();
+    }
+
+    public void OnRoll(InputAction.CallbackContext context)
+    {
+        roll1D = context.ReadValue<float>();
+    }
+
+    public void OnPitchYaw(InputAction.CallbackContext context)
+    {
+        pitchYaw = context.ReadValue<Vector2>();
+    }
+
+    #endregion
+
+}
